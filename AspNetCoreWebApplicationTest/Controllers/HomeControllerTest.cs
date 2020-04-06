503ee468960823e14e76545f734afc3e8a73309a
Suravi Mandal <53340870+suravimandal@users.noreply.github.com>
2020-04-06 22:18:29 +0800

    Update HomeControllerTest.cs

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -25,13 +25,7 @@ namespace AspNetCoreWebApplicationTest.Controllers
         }
         
         
-        [Fact]
-        public void NoInputParamGetResponseTest()
-        {
-            HelloController controller = new HelloController();
-            var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!!!", response.output);
-        }
+
       
         
     }
